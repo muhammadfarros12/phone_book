df48ad85c69f8ae7fd1c062ef3267633d1f6e038
ammaar12 <muhammadfarroos12@gmail.com>
2024-12-06 13:18:35 +0700

    styles and widget

---
+++ lib/common/styles/spacing_spaces.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:my_phone_book/constants/sizes.dart';
+
+class PSpacingSpaces {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: PSize.appBarHeight,
+    left: PSize.defaultSpace,
+    right: PSize.defaultSpace,
+    bottom: PSize.defaultSpace,
+  );  
+}
---
+++ lib/common/widges/success_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:my_phone_book/common/styles/spacing_spaces.dart';
+import 'package:my_phone_book/constants/sizes.dart';
+import 'package:my_phone_book/helper/helper_function.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+    required this.onPressed,
+  });
+
+  final String image, title, subTitle;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: PSpacingSpaces.paddingWithAppBarHeight * 2,
+          child: Column(
+            children: [
+              //? image
+              Image(
+                image: AssetImage(image),
+                width: PHelperFunction.screenWidth() * .6,
+              ),
+              const SizedBox(
+                height: PSize.spaceBtwSections,
+              ),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: PSize.spaceBtwItems,
+              ),
+              Text(
+                subTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: PSize.spaceBtwSections,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: onPressed, child: const Text('continue')),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
